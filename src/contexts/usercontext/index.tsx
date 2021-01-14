f0fc7d9f177442bd7b803f20bcc5d87c77750e1d
AngeloAlfanoc <44265793+AngeloAlfanoc@users.noreply.github.com>
2021-01-14 21:04:11 +0100

    removed head

--- src/contexts/usercontext/index.tsx
+++ src/contexts/usercontext/index.tsx
@@ -33,10 +33,7 @@ const UserProvider = ({ children }: UserProviderProps) => {
               setLoading(false);
             }
           }
-<<<<<<< HEAD
-=======
-  
->>>>>>> 436b982 (project = dead)
+
           setLoading(false);
         });
     };
